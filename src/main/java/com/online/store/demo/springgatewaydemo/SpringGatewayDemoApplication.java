c69b41bd997f547326c65d7dbf6d6b5c6f49eb73
Rajiv Srivastava <rajivmca2004@gmail.com>
2020-08-12 00:19:21 +0530

    first commit

---
+++ src/main/java/com/online/store/demo/springgatewaydemo/SpringGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.online.store.demo.springgatewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayDemoApplication.class, args);
+	}
+
+}
