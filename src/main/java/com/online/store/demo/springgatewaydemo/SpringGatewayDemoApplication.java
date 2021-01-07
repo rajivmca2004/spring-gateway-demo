8ccc4b8cf6e5b2b4fecf2e7fc21e93ae64e08594
Rajiv Srivastava <rajivmca2004@gmail.com>
2021-01-07 23:21:04 +0530

    Fixed Redis rate limiter issue

--- src/main/java/com/online/store/demo/springgatewaydemo/SpringGatewayDemoApplication.java
+++ src/main/java/com/online/store/demo/springgatewaydemo/SpringGatewayDemoApplication.java
@@ -2,6 +2,10 @@ package com.online.store.demo.springgatewaydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class SpringGatewayDemoApplication {
@@ -9,5 +13,15 @@ public class SpringGatewayDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGatewayDemoApplication.class, args);
 	}
+	
+//	@Bean
+//	KeyResolver userKeyResolver() {
+//	    return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("user"));
+//	}
+	
+	@Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.just("1");
+    }
 
 }
