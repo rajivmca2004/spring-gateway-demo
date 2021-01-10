c5d970be90d92af6e84a49aeea29bf7e807228c2
Rajiv Srivastava <rajivmca2004@gmail.com>
2021-01-10 23:31:28 +0530

    Refactor code

--- src/test/java/com/online/store/demo/springgatewaydemo/SpringGatewayDemoApplicationTests.java
+++ src/test/java/com/online/store/demo/springgatewaydemo/SpringGatewayDemoApplicationTests.java
@@ -2,7 +2,6 @@ package com.online.store.demo.springgatewaydemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class SpringGatewayDemoApplicationTests {
@@ -10,5 +9,4 @@ class SpringGatewayDemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
